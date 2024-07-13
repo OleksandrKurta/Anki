2c123050553ebf4c2ce683b62d620fb7b5fe8193
oleksandrkurta <kurta.o@ajax.systems>
2024-07-13 17:21:28 +0300

    Empty project

---
+++ src/test/kotlin/io/github/anki/Anki/AnkiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.anki.Anki
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnkiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
